da489baec4d6e863adefb7a2de42fcf4541718ea
Ashwin Bhaskaran <ashwinrookie@gmail.com>
2024-11-04 17:17:01 -0500

    cart ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppState, StudentEffects, studentReducer } from './states';
 import { environment } from 'src/environments/environment';
+import { StudentProfileComponent } from './shared/components/modals/student-profile/student-profile.component';
 
 export function appInitializerFactory(
   appInitializerService: AppInitializerService
@@ -22,7 +23,7 @@ export function appInitializerFactory(
 }
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StudentProfileComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
-
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { StudentProfileComponent } from '../modals/student-profile/student-profile.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private modalService: NgbModal) {}
+  openModal() {
+    this.modalService.open(StudentProfileComponent);
+  }
+}
